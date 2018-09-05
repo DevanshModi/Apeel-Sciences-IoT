ef2eb853e0a3c652d3da6ba873f78d4efa02b35f
Dev Modi <dxm5466@psu.edu>
2018-09-05 18:53:37 +0100

    Initial Defs

---
+++ main.py
@@ -0,0 +1,24 @@
+##Apeel Sciences Prompt Sept 10th 2018
+import time
+import serial
+import io
+
+class connector:
+    def check_serial_devices(self):
+        pass
+    
+    def read_sensors(self):
+        pass
+    
+    def classify(self):
+        pass
+    
+    def store_data(self):
+        pass
+    
+    
+    
+    
+    
+    
+    
