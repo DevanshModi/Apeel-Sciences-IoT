4237db28be4492c3ed2fab3ad84d42b6705a8026
Dev Modi <me@devansh.in>
2018-09-09 14:15:06 -0700

    Added Sqlite database and improved code structure

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 ##Apeel Sciences Prompt Sept 10th 2018
-import time, datetime, io, json, uuid, requests, boto3, serial, os
+import time, datetime, io, json, uuid, requests, boto3, serial, os, sqlite3
 from serial.tools import list_ports
 from random import randint
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 
 
+def create_connection(db_file):
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except:
+        print('sql-error')
+
+    return None
 
 class connector:
-    def __init__(self):
-        self.mode = "both"
+    def __init__(self, mode, DELAY, db, config):
+        self.actions = {"light1":False, "update_config":False}
+        self.mode = mode
         self.auth_list = []
         self.connected = set()
+        self.DELAY = DELAY #Ideally should be much longer
+        self.db_conn = create_connection(db)
 
-        with open('config.json') as f:
-            data = json.load(f)
-        if type(data['auth_list']) == int and data['auth_list']==-1:
+        with open(config) as f:
+            self.data = json.load(f)
+        if type(self.data['auth_list']) == int and self.data['auth_list']==-1:
             self.auth_list = ['ALL']
         else:
-            self.auth_list.extend(data['auth_list'])
+            self.auth_list.extend(self.data['auth_list'])
 
     #Check if any new devices are attached
     #https://pyserial.readthedocs.io/en/latest/tools.html
     #Allowed Devices = AUTHORIZED USBs
-    #TODO: Multithreading if significant change in performance
+    #TODO: Multithreading if there's significant gain in performance
     def check_serial_devices(self):
         ports = set([item for item in serial.tools.list_ports.grep("USB")])
 
@@ -34,12 +45,13 @@ class connector:
 
         self.connected = ports
 
+    #Generate Sample data for now
     def read_sensors(self):
         sample = {
             "image":self.store_image(),
             "timestamp":datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
             "humidity":randint(0,50),
-            "actions":{},
+            "actions":self.actions,
             "temp":randint(0,50),
             "connected_devices":[device.hwid for device in list(self.connected)]
         }
@@ -65,38 +77,37 @@ class connector:
 
     def callback(self, client, userdata, message):
 
-        print("Received a new message: ")
-        print(message.payload)
-        print("from topic: ")
-        print(message.topic)
-        print("--------------\n\n")
+        actions = json.loads(message.payload)
+
+        if not self.actions["light1"] and actions.get("light1") == 1:
+
+            #Real world code would enable real output pins
+            print "ALERT: Spoilage Light 1 Turned On!"
+
+            self.actions["light1"]=True
+
+        elif self.actions["light1"] and actions.get("light1") == 0:
+
+            print "ALERT: Spoilage Light 1 Turned Off!"
+
+            self.actions["light1"] = False
+
+        #if actions.get("update_config"):
+            #Do something here
 
 
+def run_instance():
+    conn = connector("both", 5, "apeel.db", "config.json")
 
-####################################
-host = 'a90y3e0ptmg5b.iot.us-east-2.amazonaws.com'
-rootCAPath = 'cert/root-CA.crt'
-certificatePath = 'cert/Vendor_Measurement-1.cert.pem'
-privateKeyPath = 'cert/Vendor_Measurement-1.private.key'
-port = 8883
-useWebsocket = False
-clientId = 'Apeel-Device-1'
-topic = '/sdk/test/Python'
-
-def test_code():
-    conn = connector()
-
-    # conn.read_sensors()
-    # Init AWSIoTMQTTClient
     myAWSIoTMQTTClient = None
-    if useWebsocket:
-        myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId, useWebsocket=True)
-        myAWSIoTMQTTClient.configureEndpoint(host, port)
-        myAWSIoTMQTTClient.configureCredentials(rootCAPath)
+    if conn.data["useWebsocket"]:
+        myAWSIoTMQTTClient = AWSIoTMQTTClient(conn.data["clientId"], useWebsocket=True)
+        myAWSIoTMQTTClient.configureEndpoint(conn.data["host"], conn.data["port"])
+        myAWSIoTMQTTClient.configureCredentials(conn.data["rootCAPath"])
     else:
-        myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
-        myAWSIoTMQTTClient.configureEndpoint(host, port)
-        myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+        myAWSIoTMQTTClient = AWSIoTMQTTClient(conn.data["clientId"])
+        myAWSIoTMQTTClient.configureEndpoint(conn.data["host"], conn.data["port"])
+        myAWSIoTMQTTClient.configureCredentials(conn.data["rootCAPath"], conn.data["privateKeyPath"], conn.data["certificatePath"])
 
     # AWSIoTMQTTClient connection configuration
     myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
@@ -105,31 +116,49 @@ def test_code():
 
     #Used to configure the draining speed to clear up the queued requests when the connection is back.
     myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
-    myAWSIoTMQTTClient.configureConnectDisconnectTimeout(5)  # 10 sec
-    myAWSIoTMQTTClient.configureMQTTOperationTimeout(10)  # 5 sec
+    myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+    myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
-    myAWSIoTMQTTClient.
     # Connect and subscribe to AWS IoT
-    myAWSIoTMQTTClient.connect()
+    try:
+        myAWSIoTMQTTClient.connect()
+    except:
+        print "Initial Connection Failed"
 
     if conn.mode == 'both' or conn.mode == 'subscribe':
-        myAWSIoTMQTTClient.subscribe(topic, 1, conn.callback)
+        myAWSIoTMQTTClient.subscribe(str(conn.data["topic_sub"]), 1, conn.callback)
 
     time.sleep(2)
 
     # Publish to the same topic in a loop forever
     loopCount = 0
     while True:
+        conn.check_serial_devices()
+
+        ##Step 1: Check existing entries in sqlite database
+        #db_rows = db_check_rows()
+
+        ##if any, publish them first and add the current sensor reading to the database (FIFO order)
+        #
+        #
         if conn.mode == 'both' or conn.mode == 'publish':
-            message = conn.read_sensors()
-            messageJson = message
-            #print message
-            myAWSIoTMQTTClient.publish(topic, messageJson, 1)
-            #print('Published topic %s: %s\n' % (topic, messageJson))
-            loopCount += 1
-        time.sleep(2)
+            try:
+                message = conn.read_sensors()
+                messageJson = message
 
-test_code()
+                # Add the current reading to database
+                ## For each row in db_rows, publish them first
+                ## Also, remember last few entries in memory to avoid duplication
 
-####################################
+                myAWSIoTMQTTClient.publish(str(conn.data["topic"]), messageJson, 1)
+                #print('Published topic %s: %s\n' % (topic, messageJson))
+                loopCount += 1
+            except:
+                #Internet disconnection
+                print "Waiting for connection..."
+                #myAWSIoTMQTTClient.connect()
+
+        time.sleep(conn.DELAY)
+
+run_instance()
 
